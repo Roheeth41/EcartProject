8b7065033842eec35ffdece5f4e55dfad53a36d7
Roheeth41 <72456682+Roheeth41@users.noreply.github.com>
2021-07-19 19:08:47 +0530

    static

--- EcartProject/urls.py
+++ EcartProject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from authentication import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,4 @@ urlpatterns = [
     path('home', views.home,name='home'),
     path('logout', views.logout,name='logout'),
     path('app/',include('ecartApp.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
