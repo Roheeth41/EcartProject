f993992852b4eabab932a15f821e4eaa63ebb973
Roheeth41 <72456682+Roheeth41@users.noreply.github.com>
2021-07-19 18:37:13 +0530

    heroku

--- EcartProject/settings.py
+++ EcartProject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-rq#l%_#ai6x$5n_6=tvx7_dv1sha13y*zc3zfaht5py*vbg=qh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://ecartapp-atoz.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1',".herokuapp.com",]
 
 # Application definition
 
